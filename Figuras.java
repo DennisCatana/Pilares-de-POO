b3bd3672582f11259d531c80e5d3f0ad85d5bd63
iveth-cocha <maria.iveth16@outlook.es>
2023-08-21 01:14:04 -0500

    Abstracion

---
+++ Figuras.java
@@ -0,0 +1,37 @@
+public class Figuras {
+    //atrinutos de el objeto figuras
+    //caracteristicas que lo componen
+    public String nombre;
+    public int n_lados;
+
+    public Figuras(String nombre, int n_lados) {
+        this.nombre = nombre;
+        this.n_lados = n_lados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getN_lados() {
+        return n_lados;
+    }
+
+    public void setN_lados(int n_lados) {
+        this.n_lados = n_lados;
+    }
+
+    public  void Imprimir(){
+        System.out.println("Figura: " + getNombre() );
+        System.out.println("# de lados: " +getN_lados() );
+
+    }
+
+    public void calcularPerimetro(){
+        System.out.println("Para calcular el perimetro se necesita conocer la longitud de los lados y el numero de lados en el caso de los Poligonos");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //abstraccion
+        Figuras figura1 =new Figuras("cuadrado", 4);
+
+        figura1.Imprimir();
+        figura1.calcularPerimetro();
+
+
+    }
+}
