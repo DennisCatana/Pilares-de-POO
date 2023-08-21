939c1459ab1aff65451b4b483643c4bbaffb1a01
iveth-cocha <maria.iveth16@outlook.es>
2023-08-21 02:47:21 -0500

    Encapsulamiento

--- Figuras.java
+++ Figuras.java
@@ -3,10 +3,14 @@ public class Figuras {
     //caracteristicas que lo componen
     public String nombre;
     public int n_lados;
+    public float longitud_lados;
+    private String color_figura;
 
-    public Figuras(String nombre, int n_lados) {
+    public Figuras(String nombre, int n_lados, float longitud_lados, String color_figura) {
         this.nombre = nombre;
         this.n_lados = n_lados;
+        this.longitud_lados = longitud_lados;
+        this.color_figura = color_figura;
     }
 
     public String getNombre() {
@@ -25,13 +29,32 @@ public class Figuras {
         this.n_lados = n_lados;
     }
 
+
+    public float getLongitud_lados() {
+        return longitud_lados;
+    }
+
+    public void setLongitud_lados(float longitud_lados) {
+        this.longitud_lados = longitud_lados;
+    }
+
+    public String getColor_figura() {
+        return color_figura;
+    }
+
+    public void setColor_figura(String color_figura) {
+        this.color_figura = color_figura;
+    }
+
     public  void Imprimir(){
         System.out.println("Figura: " + getNombre() );
         System.out.println("# de lados: " +getN_lados() );
+        System.out.println("Longitud en cm: " + getLongitud_lados() );
+        System.out.println("Color de la figura " + getColor_figura());
 
     }
     //metodos
     public void calcularPerimetro(){
-        System.out.println("Para calcular el perimetro se necesita conocer la longitud de los lados y el numero de lados en el caso de los Poligonos");
+         System.out.println("El perimetro de "+this.nombre + " es : "+longitud_lados*n_lados);
     }
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
+
+
+
+
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
         //abstraccion
-        Figuras figura1 =new Figuras("cuadrado", 4);
+        Figuras figura1 =new Figuras("cuadrado", 4, 6,"verde");
 
         figura1.Imprimir();
         figura1.calcularPerimetro();
-
 
     }
 }
